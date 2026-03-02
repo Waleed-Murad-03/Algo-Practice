477ac2b87423c10ac4df30030ba70082f12df6fb
Waleed <lido.murad@gmail.com>
2026-03-02 10:14:41 +0400

    Solved Pattern 1

--- Star-Pattern-Qs/Q1.js
+++ Star-Pattern-Qs/Q1.js
@@ -6,3 +6,13 @@ Print nxn Star Square
 * * * *
 * * * *
 */
+
+let n = 5;
+for (let i = 0; i < n; i++) {
+  let row = '';
+
+  for (let j = 0; j < n; j++) {
+    row = row + ' * ';
+  }
+  console.log(row);
+}
