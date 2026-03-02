312cf21fa01fddba72b50f361fa9500e669fd604
Waleed <lido.murad@gmail.com>
2026-03-02 10:57:29 +0400

    Solved pattern questions num 5

--- Star-Pattern-Qs/Q5.js
+++ Star-Pattern-Qs/Q5.js
@@ -6,3 +6,13 @@ Print a Reverse Right-Angled Triangle of Increasing Numbers
 1 2
 1
  */
+
+let n = 5;
+
+for (let i = 0; i < n; i++) {
+  let row = '';
+  for (let j = 0; j < n - i; j++) {
+    row = row + ' ' + (j + 1);
+  }
+  console.log(row);
+}
