e53e64ff139959a7448ae0ca15efe3311479c864
Waleed <lido.murad@gmail.com>
2026-03-02 11:29:46 +0400

    Solved pattern question 7

--- Star-Pattern-Qs/Q7.js
+++ Star-Pattern-Qs/Q7.js
@@ -6,3 +6,21 @@ Print a Right-Angled Triangle of Alternating 1s and 0s
 1 0 1
 1 0 1 0
 */
+
+let n = 4;
+
+for (let i = 0; i < n; i++) {
+  let row = '';
+  let toggle = 1;
+
+  for (let j = 0; j < i + 1; j++) {
+    row = row + toggle;
+
+    if (toggle == 1) {
+      toggle = 0;
+    } else {
+      toggle = 1;
+    }
+  }
+  console.log(row);
+}
