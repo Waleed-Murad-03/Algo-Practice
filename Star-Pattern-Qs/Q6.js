328a50786e4551ec8c3d84b6ca2ac2e0185df2cb
Waleed <lido.murad@gmail.com>
2026-03-02 11:09:30 +0400

    Solve Pattern Question num 6

--- Star-Pattern-Qs/Q6.js
+++ Star-Pattern-Qs/Q6.js
@@ -6,3 +6,17 @@ Print a Right-Aligned Right-Angled Triangle of Stars
   * * *
 * * * *
 */
+
+let n = 6;
+
+for (let i = 0; i < n; i++) {
+  let row = '';
+
+  for (let j = 0; j < n - (i + 1); j++) {
+    row = row + ' - ';
+  }
+  for (let k = 0; k < i + 1; k++) {
+    row = row + ' * ';
+  }
+  console.log(row);
+}
