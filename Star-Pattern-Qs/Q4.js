edfe8a6c0192a45a9c3f44d0542a85fea1cda109
Waleed <lido.murad@gmail.com>
2026-03-02 10:38:29 +0400

    Solved pattern question num 4

--- Star-Pattern-Qs/Q4.js
+++ Star-Pattern-Qs/Q4.js
@@ -6,3 +6,14 @@ Print a Right-Angled Triangle of Repeated Numbers
 3 3 3
 4 4 4 4
 */
+
+let n = 4;
+
+for (let i = 0; i < n; i++) {
+  let row = '';
+
+  for (let j = 0; j < i + 1; j++) {
+    row = row + ' ' + (i + 1);
+  }
+  console.log(row);
+}
