1908ce999567558289fdae1cad23835511461392
Waleed <lido.murad@gmail.com>
2026-03-02 11:50:34 +0400

    Solved pattern question number  8

--- Star-Pattern-Qs/Q8.js
+++ Star-Pattern-Qs/Q8.js
@@ -6,3 +6,17 @@ Right-Angled Triangle of Alternating 1s and 0s (Global Toggle)
 0 1 0
 1 0 1 0
 */
+
+let n = 5;
+
+let toggle = 1;
+
+for (let i = 0; i < n; i++) {
+  let row = '';
+  for (let j = 0; j < i + 1; j++) {
+    row = row + toggle;
+    toggle = toggle === 1 ? 0 : 1;
+  }
+
+  console.log(row);
+}
